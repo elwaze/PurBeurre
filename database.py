251ea18acfb4468e98f7b72e67e5494ad8d3975e
Elwaze <eloise.lamontagne@gmail.com>
2019-10-02 10:09:31 +0200

    Database connection cleaner

--- database.py
+++ database.py
@@ -1,9 +1,12 @@
 #! /usr/bin env python3
 # coding: utf-8
 
+"""
+Connection to the database.
+
+"""
+
 import records
+import os
 
-db = records.Database('mysql+mysqlconnector://root:Uhqjr#2019@localhost/OFF')
-
-# var d'environnement pour ne pas écrire mon mdp en clair.
-# => "mysql://username:{}@hostname:port/table".format(os.environ.get('P5_MYSQL_PASSWORD')
+db = records.Database('mysql+mysqlconnector://root:{}@localhost/OFF'.format(os.environ.get('P5_MYSQL_PASSWORD')))
